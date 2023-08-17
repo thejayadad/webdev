e32f156a80be43fffc8b38619bd338d1a35f4826
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-08-17 11:03:07 -0400

    update

---
+++ pyramid/main.js
@@ -0,0 +1,22 @@
+const taskInput = document.getElementById('taskInput');
+const addButton = document.getElementById('addButton');
+const taskList = document.getElementById('taskList');
+
+addButton.addEventListener('click', () => {
+  if (taskInput.value.trim() !== '') {
+    const taskText = taskInput.value;
+    taskInput.value = '';
+
+    const li = document.createElement('li');
+    li.innerHTML = `
+      ${taskText}
+      <button>Delete</button>
+    `;
+
+    taskList.appendChild(li);
+
+    li.querySelector('button').addEventListener('click', () => {
+      taskList.removeChild(li);
+    });
+  }
+});
