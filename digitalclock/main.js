0a0b9b36fe46773e5c49cabd1c2ad1b5362f35fb
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-08-22 04:15:55 -0400

    update

--- digitalclock/main.js
+++ digitalclock/main.js
@@ -1,12 +1,12 @@
 function updateClock() {
-    // const now = new Date();
-    // const hours = now.getHours().toString().padStart(2, '0');
-    // const minutes = now.getMinutes().toString().padStart(2, '0');
-    // const seconds = now.getSeconds().toString().padStart(2, '0');
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
 
-    // document.getElementById('hours').textContent = hours;
-    // document.getElementById('minutes').textContent = minutes;
-    // document.getElementById('seconds').textContent = seconds;
+    document.getElementById('hours').textContent = hours;
+    document.getElementById('minutes').textContent = minutes;
+    document.getElementById('seconds').textContent = seconds;
 }
 
 updateClock();
