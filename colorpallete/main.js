af38a7b5e1212015acfe91bd3cb1f790c35d0bdd
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-08-28 19:10:34 -0400

    update

--- colorpallete/main.js
+++ colorpallete/main.js
@@ -1,30 +1,26 @@
-const colorPalette = document.getElementById("colorPalette");
-const generateButton = document.getElementById("generateButton");
 
-generateButton.addEventListener("click", generatePalette);
-
-function generatePalette() {
+const colorPalette = document.getElementById("colorPalette")
+const generateButton = document.getElementById("generateButton")
+generateButton.addEventListener("click", generatePalette)
+function generatePalette(){
   colorPalette.innerHTML = "";
-  for (let i = 0; i < 5; i++) {
-    const color = getRandomColor();
-    const colorBox = document.createElement("div");
-    colorBox.classList.add("color-box");
+  for(let i = 0; i < 5; i++){
+    const color = getRandomColor()
+    const colorBox = document.createElement("div")
+    colorBox.classList.add("color-box")
     colorBox.style.backgroundColor = color;
-
-    const colorCode = document.createElement("p");
-    colorCode.classList.add("color-code");
+    const colorCode = document.createElement("p")
+    colorCode.classList.add("color-code")
     colorCode.textContent = color;
-
-    colorBox.appendChild(colorCode);
-    colorPalette.appendChild(colorBox);
+    colorBox.appendChild(colorCode)
+    colorPalette.appendChild(colorBox)
   }
 }
-
-function getRandomColor() {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+function getRandomColor(){
+  const letters = "0123456789ABCDEF"
+  let color = "#"
+  for(let i = 0; i < 6; i++){
+    color += letters[Math.floor(Math.random() * 16)]
   }
-  return color;
+  return color
 }
