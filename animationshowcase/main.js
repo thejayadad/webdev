4e3754b592e626193235dbccbb1b036dc826703b
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-09-06 11:38:42 -0400

    update

--- animationshowcase/main.js
+++ animationshowcase/main.js
@@ -1,22 +1,20 @@
-const steps = document.querySelectorAll('.step');
-let currentStep = 0;
 
-function showStep(step) {
-  steps[currentStep].style.display = 'none';
-  steps[step].style.display = 'flex';
-  currentStep = step;
+const steps = document.querySelectorAll('.step')
+let currentStep = 0
+function showStep(step){
+    steps[currentStep].style.display = 'none';
+    steps[step].style.display = 'flex';
+    currentStep = step;
 }
-
-showStep(currentStep);
-
+showStep(currentStep)
 document.addEventListener('keydown', (event) => {
-  if (event.key === 'ArrowRight') {
-    if (currentStep < steps.length - 1) {
-      showStep(currentStep + 1);
+    if(event.key === 'ArrowRight'){
+        if(currentStep <  steps.length - 1){
+            showStep(currentStep + 1)
+        }
+    } else if (event.key === 'ArrowLeft'){
+        if(currentStep > 0){
+            showStep(currentStep - 1)
+        }
     }
-  } else if (event.key === 'ArrowLeft') {
-    if (currentStep > 0) {
-      showStep(currentStep - 1);
-    }
-  }
-});
+})
