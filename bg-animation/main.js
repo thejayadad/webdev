f2282c219c8ec263781eecb2ac257422858169c3
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-09-06 12:42:28 -0400

    update

--- bg-animation/main.js
+++ bg-animation/main.js
@@ -1,26 +1,22 @@
-const particleContainer = document.querySelector(".particle-container");
 
-function createParticle() {
-  const particle = document.createElement("div");
-  particle.className = "particle";
+const particleContainer = document.querySelector(".particle-container")
+function createParticle(){
+    const particle = document.createElement("div")
+    particle.className = "particle";
+    const size = Math.random() * 20 + 5 + "px";
+    particle.style.width = size;
+    particle.style.height = size;
+    const top = Math.random() *window.innerHeight + "px";
+    const left = Math.random() *window.innerWidth + "px";
+    particle.style.top = top;
+    particle.style.left = left;
+    const animationDuration = Math.random() *2+1+"s";
+    particle.style.animationDuration = animationDuration;
+    particleContainer.appendChild(particle)
 
-  const size = Math.random() * 20 + 5 + "px";
-  particle.style.width = size;
-  particle.style.height = size;
+    setTimeout(() => {
+        particle.remove();
 
-  const top = Math.random() * window.innerHeight + "px";
-  const left = Math.random() * window.innerWidth + "px";
-  particle.style.top = top;
-  particle.style.left = left;
-
-  const animationDuration = Math.random() * 2 + 1 + "s";
-  particle.style.animationDuration = animationDuration;
-
-  particleContainer.appendChild(particle);
-
-  setTimeout(() => {
-    particle.remove();
-  }, parseFloat(animationDuration) * 1000);
+    }, parseFloat(animationDuration) * 1000);
 }
-
 setInterval(createParticle, 200);
