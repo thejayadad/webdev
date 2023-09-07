a9a9ba8f2bf220af5b6e680d07d8f58614b18d47
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-09-07 12:17:40 -0400

    update

--- countdown/main.js
+++ countdown/main.js
@@ -1,29 +1,34 @@
- const coundownForm = document.getElementById("countdownForm")
- const countdownDateInput = document.getElementById("countdownDate")
- const countdownDisplay = document.getElementById("countdown")
- let countdownInterval
+const countdownForm = document.getElementById("countdownForm");
+const countdownDateInput = document.getElementById("countdownDate");
+const countdownDisplay = document.getElementById("countdown");
+let countdownInterval;
 
- coutdownForm.addEventListener("submit", startCountdown)
+countdownForm.addEventListener("submit", startCountdown);
 
- function startCountdown(e){
-    e.preventDefault()
-    const countdownDate = new Date(countdownDateInput.value).getTime()
-    if(isNaN(countdownDate)){
-        alert("Please select a valid date and Time")
-        return
+function startCountdown(e) {
+    e.preventDefault();
+    const countdownDate = new Date(countdownDateInput.value).getTime();
+    if (isNaN(countdownDate)) {
+        alert("Please select a valid date and time");
+        return;
     }
-    countdownInterval = setInterval(updateCountdown, 1000)
-    function updateCountdown(){
-        const now = new Date().getTime()
+    countdownInterval = setInterval(updateCountdown, 1000);
+
+    function updateCountdown() {
+        const now = new Date().getTime();
         const timeRemaining = countdownDate - now;
-        if(timeRemaining <= 0){
-            clearInterval(countdownInterval)
-            countdownDisplay.textContent = "Event Time Has Arrived"
-        } else{
-            const hours = Math.floor(timeRemaining / (1000 * 60 ^))
-            const minutes = Math.floor((timeRemaining & (1000 * 60 * 60)) / (1000 * 60))
-            const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000)
-            countdownDisplay.textContent = `${formatTime(hours) : ${formatTime(minutes)}}`
+        if (timeRemaining <= 0) {
+            clearInterval(countdownInterval);
+            countdownDisplay.textContent = "Event Time Has Arrived";
+        } else {
+            const hours = Math.floor(timeRemaining / (1000 * 60 * 60));
+            const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+            countdownDisplay.textContent = `${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)}`;
         }
     }
- }
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
