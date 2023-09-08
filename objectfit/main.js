7b29ff2de8e9e9fa25e22e1d9581c0572f16e257
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-09-08 10:38:06 -0400

    update

---
+++ objectfit/main.js
@@ -0,0 +1,17 @@
+function changeImage(style) {
+    const image = document.getElementById('review-image');
+    switch (style) {
+        case 'fill':
+            image.style.objectFit = 'fill';
+            break;
+        case 'contain':
+            image.style.objectFit = 'contain';
+            break;
+        case 'cover':
+            image.style.objectFit = 'cover';
+            break;
+        case 'none':
+            image.style.objectFit = 'none';
+            break;
+    }
+}
